71f59f5276f6c204b495372bb59636e4098c53e6
24958 <249583491@qq.com>
2020-03-10 22:48:17 +0800

    33

--- all/src/Search.java
+++ all/src/Search.java
@@ -17,14 +17,31 @@ public class Search {
     }
 
 
-    public static int LeftBoundSearch(){
-
-        return -1;
+    public static int LeftBoundSearch(int[] nums, int target){
+        int left = 0, right = nums.length - 1;
+        // 搜索区间为 [left, right]
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                // 搜索区间变为 [mid+1, right]
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                // 搜索区间变为 [left, mid-1]
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 收缩右侧边界
+                right = mid - 1;
+            }
+        }
+        // 检查出界情况
+        if (left >= nums.length || nums[left] != target)
+            return -1;
+        return left;
     }
 
 
     public static void main(String[] args) {
         int[] array = {1,3,5,7,8};
-        System.out.println(BinarySearch(array,7));
+        System.out.println(LeftBoundSearch(array,8));
     }
 }
