76e4add875dd6d9394d9dfc816516e26dfaea4c2
yu2.guo <yu2.guo@ly.com>
2020-03-24 17:48:27 +0800

    BST add &&  infixSearch && del

--- dds/src/Tree/BinarySearchTree.java
+++ dds/src/Tree/BinarySearchTree.java
@@ -39,10 +39,214 @@ parent.left = null || parent.right = null
 *删除最小节点
 *targetNode.value = temp
 
-
-
-
  */
 
 public class BinarySearchTree {
+
+    private BSTNode head;
+
+    //BST添加
+    public void add(BSTNode node) {
+        if(head == null){
+            head = node;
+        } else {
+            head.add(node);
+        }
+    }
+
+    //BST中序遍历
+    public void infixShow(){
+        if(head == null){
+            return;
+        }
+        this.head.infixShow();
+    }
+
+    //BST二分查找目标节点
+    public BSTNode Search(int target){
+        if(head == null){
+            return null;
+        }
+        return head.Search(target);
+    }
+
+    //BST二分查找目标节点的父节点
+    public BSTNode SearchParent(int target){
+        if(head == null){
+            return null;
+        }
+        return head.SearchParent(target);
+    }
+
+    //BST删除目标节点
+    public void delete(int data){
+        //1.首先找到该节点，判断节点类型（叶子节点、单子树节点、双子树节点）
+        if(this.head.data == data){
+            this.head = null;
+            return;
+        }
+        BSTNode targetNode = head.Search(data);
+        BSTNode parentNode = head.SearchParent(data);
+        //若为叶子节点
+        if(targetNode.left == null && targetNode.right == null){
+            //确定为parent的左子节点还是右子节点
+            if(parentNode.left.equals(targetNode)){
+                parentNode.left = null;
+            } else {
+                parentNode.right = null;
+            }
+            return;
+        }
+        //若为双子树节点，（左右节点其都为null）
+        else if(targetNode.left != null && targetNode.right != null){
+            //从目标节点的右子树找到最小的节点
+            BSTNode minNode = targetNode.right.SearchMin();
+            int temp = minNode.data;
+            minNode = null;
+            targetNode.data = temp;
+            return;
+        }
+        else {
+            //目标存在左子树
+            if(targetNode.left != null){
+                //目标节点在父节点的左
+                if(parentNode.left.equals(targetNode)){
+                    parentNode.left = targetNode.left;
+                } else {
+                    parentNode.right = targetNode.left;
+                }
+            }
+            //目标存在右子树
+            else {
+                //目标节点在父节点的左
+                if(parentNode.left.equals(targetNode)){
+                    parentNode.left = targetNode.right;
+                } else {
+                    parentNode.right = targetNode.right;
+                }
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args){
+        BinarySearchTree bst = new BinarySearchTree();
+        int[] test = {1,2,5,3,4,6};
+        for (int i = 0; i < test.length; i++){
+            bst.add(new BSTNode(test[i]));
+        }
+        bst.infixShow();
+    }
+
+
+}
+
+class BSTNode{
+    int data;
+    BSTNode left;
+    BSTNode right;
+
+    @Override
+    public String toString() {
+        return "BSTNode{" +
+                "data=" + data +
+                '}';
+    }
+
+    //构造方法
+    public BSTNode(int _data){
+        this.data = _data;
+        this.left = null;
+        this.right = null;
+    }
+
+    //二叉排序规则构建BST
+    public void add(BSTNode node){
+        if(node.data > this.data){
+            if(this.right !=null){
+                this.right.add(node);
+            } else {
+                this.right = node;
+            }
+        } else if(node.data <= this.data) {
+            if(this.left != null){
+                this.left.add(node);
+            } else {
+                this.left = node;
+            }
+        }
+    }
+
+    //中序遍历
+    public void infixShow(){
+        if(this.left != null){
+            this.left.infixShow();
+        }
+        System.out.print(this.data + " ");
+        if(this.right != null){
+            this.right.infixShow();
+        }
+    }
+
+    //中序查找目标节点，并返回当前节点
+    public BSTNode infixSearch(int target){
+        BSTNode res = null;
+        if(this.left != null){
+            res = this.left.infixSearch(target);
+        }
+        if (res != null){
+            return res;
+        }
+        if(this.data == target){
+            return this;
+        }
+        if(this.right != null){
+            res = this.right.infixSearch(target);
+        }
+        return res;
+    }
+
+    //目标节点（二分查找）
+    public BSTNode Search(int target){
+        BSTNode tmpNode = this;
+        while(tmpNode != null){
+            if(target == tmpNode.data){
+                return tmpNode;
+            } else if (target > tmpNode.data) {
+                tmpNode = tmpNode.right;
+            } else {
+                tmpNode = tmpNode.left;
+            }
+        }
+        return null;
+    }
+
+    //目标节点的父节点（二分查找）
+    public BSTNode SearchParent(int target){
+        BSTNode tmpNode = this;
+        if(this.data == target){
+            return null;
+        }
+        while(tmpNode.left != null || tmpNode.right != null){
+            if(tmpNode.left.data == target || tmpNode.right.data == target){
+                return tmpNode;
+            }
+            if(target > tmpNode.data){
+                tmpNode = tmpNode.right;
+            } else if(target < tmpNode.data){
+                tmpNode = tmpNode.left;
+            }
+        }
+        return null;
+    }
+
+    //找到最小节点
+    public BSTNode SearchMin(){
+        BSTNode minNode = this;
+        while(minNode.left != null){
+            minNode = minNode.left;
+        }
+        return minNode;
+    }
 }
